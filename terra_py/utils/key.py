5e7fa872ed55b395da8d15c7b8bc8ff0244cbb20
etienne-napoleone <etienne@terra.money>
2019-08-08 15:35:38 +0900

    set lang via params

--- terra_py/utils/key.py
+++ terra_py/utils/key.py
@@ -3,7 +3,7 @@ import secrets
 from btclib import bip39
 
 
-def generate_mnemonic(entropy_length: int = 256) -> str:
+def generate_mnemonic(entropy_length: int = 256, lang: str = 'en') -> str:
     """Generates a bip39 mnemonic based on 256 bits of entropy
 
     Uses the `secrets` module introduced in python 3.6.
@@ -11,10 +11,10 @@ def generate_mnemonic(entropy_length: int = 256) -> str:
     designed for security/cryptography.
     """
     entropy = secrets.randbits(entropy_length)
-    return bip39.mnemonic_from_entropy(entropy, 'en')
+    return bip39.mnemonic_from_entropy(entropy, lang)
 
 
-def validate_mnemonic(mnemonic: str, lang: str) -> bool:
+def validate_mnemonic(mnemonic: str, lang: str = 'en') -> bool:
     """Validate a mnemonic by trying to get its entropy"""
     try:
         bip39.entropy_from_mnemonic(mnemonic, lang)
