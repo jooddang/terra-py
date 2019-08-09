f1a38f7e672f7f6919dd10aa0043abde7e25747f
etienne-napoleone <etienne@terra.money>
2019-08-09 11:26:12 +0900

    add tests for derive_master_key

--- tests/test_utils_key.py
+++ tests/test_utils_key.py
@@ -1,13 +1,27 @@
+import pytest
+
 from terra_py.utils import key
 
 mnemonic = ('bread genuine element reopen cliff power mean quiz mutual '
             'six machine planet dry detect edit slim clap firm jelly '
             'success narrow orange echo tomorrow')
+bad_mnemonic = 'this is a bad mnemonic'
 
 
 def test_validate_mnemonic():
     assert key.validate_mnemonic(mnemonic)
-    assert not key.validate_mnemonic('wrong mnemonic')
+    assert not key.validate_mnemonic(bad_mnemonic)
+
+
+def test_derive_master_key():
+    mnemonic = key.generate_mnemonic()
+    master_key = key.derive_master_key(mnemonic)
+    assert master_key[:4] == b'xprv'
+
+
+def test_derive_master_key_raise_valueerror():
+    with pytest.raises(ValueError):
+        assert key.derive_master_key(bad_mnemonic)
 
 
 def test_generate_mnemonic():
