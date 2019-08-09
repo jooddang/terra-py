7b64ec111f5eed56863047ec784b0209c7554056
etienne-napoleone <etienne@terra.money>
2019-08-09 15:34:26 +0900

    improve docstrings

--- terra_py/utils/key.py
+++ terra_py/utils/key.py
@@ -7,7 +7,7 @@ KeyPair = namedtuple('KeyPair', ['private_key', 'public_key'])
 
 
 def validate_mnemonic(mnemonic: str, lang: str = 'en') -> bool:
-    """Validate a mnemonic by trying to get its entropy"""
+    """Validate a mnemonic by trying to get its entropy."""
     try:
         bip39.entropy_from_mnemonic(mnemonic, lang)
         return True
@@ -16,7 +16,7 @@ def validate_mnemonic(mnemonic: str, lang: str = 'en') -> bool:
 
 
 def derive_master_key(mnemonic: str) -> bytes:
-    """Derive a master key from a given mnemonic"""
+    """Derive a master key from a given mnemonic."""
     if validate_mnemonic(mnemonic):
         # empty string for no passphrase
         return bip39.mprv_from_mnemonic(mnemonic, '', bip32.MAINNET_PRV)
@@ -25,7 +25,9 @@ def derive_master_key(mnemonic: str) -> bytes:
 
 
 def derive_key_pair(master_key: bytes, account: int = 0, index: int = 0) -> KeyPair:  # noqa: E501
-    """Derive a key pair (priv + pub key) from a given master key"""
+    """Derive an extended key pair (extended priv + extended pub key)
+    using luna HD path from a given master key.
+    """
     luna_hd_path = f'm/44\'/330\'/{account}\'/0/{index}'
     private_key = bip32.derive(master_key, luna_hd_path)
     public_key = bip32.xpub_from_xprv(private_key)
@@ -33,7 +35,7 @@ def derive_key_pair(master_key: bytes, account: int = 0, index: int = 0) -> KeyP
 
 
 def generate_mnemonic(entropy_length: int = 256, lang: str = 'en') -> str:
-    """Generates a bip39 mnemonic based on 256 bits of entropy
+    """Generates a bip39 mnemonic based on 256 bits of entropy.
 
     Uses the `secrets` module introduced in python 3.6.
     It should be used instead of the `random` module which is not
