88ae04dec00d4bc0ed23a21ace09614d7ec6713e
etienne-napoleone <etienne@terra.money>
2019-08-12 15:53:47 +0900

    add tests for MsgSend

--- tests/test_msg.py
+++ tests/test_msg.py
@@ -1,6 +1,7 @@
 import json
 
 from terra import msg
+from terra.msg.msgsend import MsgSendValue
 from terra.msg.stdtx import StdTxValue
 
 STD_TX = {
@@ -17,6 +18,18 @@ STD_TX = {
         'msg': [],
         'signatures': [],
     },
+}
+
+MSG_SEND = {
+    'type': 'pay/MsgSend',
+    'value': {
+        'amount': [{
+            'denom': 'uluna',
+            'amount': '1000',
+        }],
+        'from_address': 'terra321',
+        'to_address': 'terra123',
+    }
 }
 
 
@@ -43,3 +56,23 @@ def test_stdtx():
     fee = msg.Fee(gas='500', amount=[amount])
     value = msg.StdTx(fee=fee, memo='test', msg=[], signatures=[])
     assert value.to_json() == json.dumps(STD_TX)
+
+
+def test_msgsendvalue():
+    amount = msg.Amount(amount='1000', denom='uluna')
+    value = MsgSendValue(
+        amount=[amount],
+        from_address='terra321',
+        to_address='terra123',
+    )
+    assert value.to_json() == json.dumps(MSG_SEND['value'])
+
+
+def test_msgsend():
+    amount = msg.Amount(amount='1000', denom='uluna')
+    value = msg.MsgSend(
+        amount=[amount],
+        from_address='terra321',
+        to_address='terra123',
+    )
+    assert value.to_json() == json.dumps(MSG_SEND)
