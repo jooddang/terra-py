3b66dedbc793905f381b6af14dbb67a0df016c5b
etienne-napoleone <etienne@terra.money>
2019-08-12 20:01:04 +0900

    add oracle to msg module

--- terra/msg/__init__.py
+++ terra/msg/__init__.py
@@ -1,5 +1,5 @@
 from terra.msg.amount import Amount
 from terra.msg.fee import Fee
-from terra.msg import auth, pay
+from terra.msg import auth, oracle, pay
 
-__all__ = ['Amount', 'Fee', 'auth', 'pay']
+__all__ = ['Amount', 'Fee', 'auth', 'oracle', 'pay']
