f6eee60d7e9de55ddfad56c7c4bcc7da026be287
etienne-napoleone <etienne@terra.money>
2019-08-12 20:15:27 +0900

    add msgpricevote to oracle

---
+++ terra/msg/oracle/msgpricevote.py
@@ -0,0 +1,41 @@
+from terra.utils import JsonSerializable
+
+
+class MsgPriceVote(JsonSerializable):
+
+    def __init__(
+        self,
+        price: str,
+        salt: str,
+        denom: str,
+        feeder: str,
+        validator: str,
+    ) -> None:
+        """Represent the top level of a MsgPriceVote message.
+
+        If a hash_ is provided, it will override price and salt.
+        """
+        self.type = 'oracle/MsgPriceVote'
+        self.value = MsgPriceVoteValue(
+            price,
+            salt,
+            denom,
+            feeder,
+            validator,
+        )
+
+
+class MsgPriceVoteValue(JsonSerializable):
+
+    def __init__(
+        self,
+        price: str,
+        salt: str,
+        denom: str,
+        feeder: str,
+        validator: str,
+    ) -> None:
+        """Values of a MsgPriceVote message."""
+        self.denom = denom
+        self.feeder = feeder
+        self.validator = validator
