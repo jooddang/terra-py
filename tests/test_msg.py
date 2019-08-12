77918f5b8142727e48d9b090e5b77bfbf58b43c1
etienne-napoleone <etienne@terra.money>
2019-08-12 12:49:30 +0900

    add test for Fee

--- tests/test_msg.py
+++ tests/test_msg.py
@@ -4,3 +4,9 @@ from terra import msg
 def test_amount():
     amount = msg.Amount(amount='1000', denom='uluna')
     assert amount.to_json() == '{"denom": "uluna", "amount": "1000"}'
+
+
+def test_fee():
+    amount = msg.Amount(amount='1000', denom='uluna')
+    fee = msg.Fee(gas='500', amount=[amount])
+    assert fee.to_json() == '{"gas": "500", "amount": [{"denom": "uluna", "amount": "1000"}]}'  # noqa: E501
