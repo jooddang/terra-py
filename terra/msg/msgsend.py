77c764adcab2f43871f6976f4a56eda00f750d77
etienne-napoleone <etienne@terra.money>
2019-08-12 15:23:44 +0900

    update type hints

--- terra/msg/msgsend.py
+++ terra/msg/msgsend.py
@@ -4,7 +4,12 @@ from terra.utils import JsonSerializable
 
 class MsgSend(JsonSerializable):
 
-    def __init__(self, amount: [Amount], from_address: str, to_address: str):
+    def __init__(
+        self,
+        amount: [Amount],
+        from_address: str,
+        to_address: str
+    ) -> None:
         """Represent the top level of a MsgSend message."""
         self.type = 'pay/MsgSend'
         self.value = MsgSendValue(amount, from_address, to_address)
@@ -12,7 +17,12 @@ class MsgSend(JsonSerializable):
 
 class MsgSendValue(JsonSerializable):
 
-    def __init__(self, amount: [Amount], from_address: str, to_address: str):
+    def __init__(
+        self,
+        amount: [Amount],
+        from_address: str,
+        to_address: str
+    ) -> None:
         """Values of a MsgSend message."""
         self.amount = sorted(amount, key=lambda o: o.denom)
         self.from_address = from_address
