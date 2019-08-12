0f9ecb434377e48f69912eceedf1094f40ea405a
etienne-napoleone <etienne@terra.money>
2019-08-12 13:38:17 +0900

    add MsgSend and MsgSendValue classes

---
+++ terra/msg/msgsend.py
@@ -0,0 +1,19 @@
+from terra.msg import Amount
+from terra.utils import JsonSerializable
+
+
+class MsgSend(JsonSerializable):
+
+    def __init__(self, amount: [Amount], from_address: str, to_address: str):
+        """Represent the top level of a StdTx message."""
+        self.type = 'pay/MsgSend'
+        self.value = MsgSendValue(amount, from_address, to_address)
+
+
+class MsgSendValue(JsonSerializable):
+
+    def __init__(self, amount: [Amount], from_address: str, to_address: str):
+        """Values of a StdTx message."""
+        self.amount = amount
+        self.from_address = from_address
+        self.to_address = to_address
