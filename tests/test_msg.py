aada91bf768af4ffda23dbb8230ebd4a009c12e1
etienne-napoleone <etienne@terra.money>
2019-08-12 13:03:47 +0900

    add tests for StdTx

--- tests/test_msg.py
+++ tests/test_msg.py
@@ -1,4 +1,5 @@
 from terra import msg
+from terra.msg.stdtx import StdTxValue
 
 
 def test_amount():
@@ -10,3 +11,17 @@ def test_fee():
     amount = msg.Amount(amount='1000', denom='uluna')
     fee = msg.Fee(gas='500', amount=[amount])
     assert fee.to_json() == '{"gas": "500", "amount": [{"denom": "uluna", "amount": "1000"}]}'  # noqa: E501
+
+
+def test_stdtxvalue():
+    amount = msg.Amount(amount='1000', denom='uluna')
+    fee = msg.Fee(gas='500', amount=[amount])
+    value = StdTxValue(fee=fee, memo='test', msg=[], signatures=[])
+    assert value.to_json() == '{"fee": {"gas": "500", "amount": [{"denom": "uluna", "amount": "1000"}]}, "memo": "test", "msg": [], "signatures": []}'  # noqa: E501
+
+
+def test_stdtx():
+    amount = msg.Amount(amount='1000', denom='uluna')
+    fee = msg.Fee(gas='500', amount=[amount])
+    value = msg.StdTx(fee=fee, memo='test', msg=[], signatures=[])
+    assert value.to_json() == '{"type": "auth/StdTx", "value": {"fee": {"gas": "500", "amount": [{"denom": "uluna", "amount": "1000"}]}, "memo": "test", "msg": [], "signatures": []}}'  # noqa: E501
