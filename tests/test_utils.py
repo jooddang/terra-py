dad7bd55cbd7117d7bcd6151e8cee02334edca30
etienne-napoleone <etienne@terra.money>
2019-08-22 14:41:37 +0900

    add ecdsa sign to crypto utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -20,3 +20,12 @@ def test_generate_salt():
     salt = crypto.generate_salt()
     assert len(salt) == 4
     assert salt != crypto.generate_salt()
+
+
+def test_sign_payload():
+
+    signature = crypto.sign(
+        payload='123',
+        private_key='861c3746d1bf6bc83acac4c9e72dbe7cdcf944031823b1c7e1248d163c2b9c01',  # noqa: E501
+    )
+    assert signature.hex() == '280bf235bc97d4a98b64af088ebc090234e0749d0f9128367c6bea52853b4a682ec32c35430a0f7256e0fcb26e8082b30b86da37fbcced5b644728b9dd3c4539'  # noqa: E501
