3553b0c9091f8d5df794f7d32f6ace26fb44744e
etienne-napoleone <etienne@terra.money>
2019-08-26 16:39:26 +0900

    add inout class

--- terra/msg/__init__.py
+++ terra/msg/__init__.py
@@ -1,6 +1,7 @@
 from terra.msg import auth, oracle, pay, staking
 from terra.msg.coin import Coin
 from terra.msg.fee import Fee
+from terra.msg.inout import InOut
 from terra.msg.tx import Tx
 
-__all__ = ["auth", "oracle", "pay", "staking", "Coin", "Fee", "Tx"]
+__all__ = ["auth", "oracle", "pay", "staking", "Coin", "Fee", "InOut", "Tx"]
