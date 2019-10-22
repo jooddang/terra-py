83fc8800ca34d714a289034ae7cf5931002191f7
etienne-napoleone <etienne@terra.money>
2019-10-22 12:32:01 +0900

    tests(api): add tests for auth

--- tests/test_api.py
+++ tests/test_api.py
@@ -126,3 +126,8 @@ def test_get_tendermint_blocks_latest():
 
 def test_get_oracle_denoms_actives():
     assert list(api.oracle.denoms.actives.get().keys())[0] == "actives"
+
+
+def test_get_auth_accounts_by_address():
+    address = "terra1d03dz5n3hj8qfzfjvrza8a9t0hejwnjcdsn5cw"
+    assert list(api.auth.accounts.by_address.get(address).keys())[0] == "type"
